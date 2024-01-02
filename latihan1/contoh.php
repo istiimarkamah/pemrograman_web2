fe5d0a81e5836621ff712790d852178e0e0f412d
istiimarkamah <148844692+istiimarkamah@users.noreply.github.com>
2024-01-02 13:46:07 +0700

    Add files via upload

---
+++ latihan1/contoh.php
@@ -0,0 +1,14 @@
+<?php
+echo "Looping menggunakan For : </br>";
+for ($i=0; $i <=10 ; $i++) { 
+    echo "<li> baris $i</li";
+}
+echo "</br>"
+echo "Looping menggunakan while";
+$i=1;
+while ($a <= 10) {
+    echo "<li> Urutan $i</li>";
+
+}
+
+?>
---
+++ latihan1/variable_tipeData.php
@@ -0,0 +1,14 @@
+<?php
+
+$nim = "701220131";
+$nama = " Isti Markamah";
+$umur = 19;
+$nilai = 90.90;
+
+echo "Nim : ". $nim . "</br>";
+echo "Nama : $nama</br>";
+print "Umur : ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("Nilai : %.3f</br>", $nilai);
+
+?>
