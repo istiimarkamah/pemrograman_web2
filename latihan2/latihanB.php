9121f2b64b54f0f8678142766f8f4824abff2f8e
istiimarkamah <148844692+istiimarkamah@users.noreply.github.com>
2024-01-02 13:46:59 +0700

    Add files via upload

---
+++ latihan2/latihanB.php
@@ -0,0 +1,18 @@
+<?php
+
+$x = 10;
+
+echo "Aku adalah angka $x </br>";
+
+$hasilKali = $x * 8;
+echo "Jika Aku di kali 8, jumlahku sekarang $hasilKali </br>";
+
+
+$hasilBagi = $hasilKali / 4;
+echo "Jika Aku di bagi 4, jumlahku skarang $hasilBagi </br>";
+
+$hasilKurang = $hasilBagi - 6;
+echo "Jika Aku di kurang 6, jumlahku sekarang $hasilKurang </br>";
+
+$hasilTambah = $hasilKurang + 2;
+echo "Jika Aku di tambah 2, jumlahku sekarang $hasilTambah </br>";
